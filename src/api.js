f0470272e16cdcf38f581398e3236ff2e2c1faa0
Philipp Wambach <philippwambach@googlemail.com>
2018-01-18 00:01:46 +0100

    add dev url

--- src/api.js
+++ src/api.js
@@ -1,10 +1,11 @@
-import {BASE_URL} from './config';
+import {API_URL} from './config';
 
 export function loadBlocks(youngestBlockNumber) {
-  const url = new URL(`${window.location.origin}/${BASE_URL}/blocks`);
+  const url = new URL(API_URL);
 
   if (youngestBlockNumber) {
     url.searchParams.append('last', youngestBlockNumber);
+    url.searchParams.append('cb', Date.now());
   }
 
   return fetch(url).then(res => res.json());
--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
-export const BASE_URL = 'api';
+export const API_URL = process.env.NODE_ENV === 'production' ?
+  `${window.location.origin}/api/blocks` :
+  'http://localhost:8081/api/blocks';
 export const REFRESH_TIME = 1000 * 5;
 export const TX_BLACK_ETHER = 20;
 export const CHAIN_MARGIN_TOP = 20;
