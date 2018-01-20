219c98545f3f4acb2617dd5d895896ad28fa619f
Philipp Wambach <philippwambach@googlemail.com>
2018-01-20 16:37:47 +0100

    move menu right and handle animation better

--- src/components/menu-icon/menu-icon.js
+++ src/components/menu-icon/menu-icon.js
@@ -1,13 +1,43 @@
-import React from 'react'; // eslint-disable-line no-unused-vars
+import React, {Component} from 'react'; // eslint-disable-line no-unused-vars
 import './menu-icon.css';
 
-export default ({open, onClick}) => (
-  <div
-    onClick={() => onClick()}
-    className={`menu-icon ${open ? 'menu-icon--open' : ''}`}
-  >
-    <span />
-    <span />
-    <span />
-  </div>
-);
+export default class MenuIcon extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {closing: false};
+  }
+
+  handleClick() {
+    const {open} = this.props;
+
+    this.props.onClick();
+
+    if (open) {
+      this.setState({closing: true});
+      setTimeout(() => this.setState({closing: false}), 1000);
+    }
+  }
+
+  render() {
+    const {open} = this.props;
+    const {closing} = this.state;
+    const classes = [
+      'menu-icon',
+      open && 'menu-icon--open',
+      closing && 'menu-icon--closing'
+    ]
+      .filter(Boolean)
+      .join(' ');
+
+    return (
+      <div
+        onClick={() => this.handleClick()}
+        className={classes}
+      >
+        <span />
+        <span />
+        <span />
+      </div>
+    );
+  }
+}
