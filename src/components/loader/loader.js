c551ee63c1858b0add398a28efc578a6bcc195c8
Philipp Wambach <philippwambach@googlemail.com>
2018-12-20 18:47:43 +0100

    chore(dependencies): update

---
+++ src/components/loader/loader.js
@@ -0,0 +1,9 @@
+import React from 'react'; // eslint-disable-line no-unused-vars
+import './loader.css';
+
+export default () =>
+  <div className="loader">
+    <div className="loader__rect" />
+    <div className="loader__rect" />
+    <div className="loader__rect" />
+  </div>;
